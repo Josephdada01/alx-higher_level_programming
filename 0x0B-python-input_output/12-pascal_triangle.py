7013dd37044e103e092f7ccd91b3e7b3566afeeb
Josephdada01 <ridwandada500@gmail.com>
2023-09-13 08:43:34 -0700

    final task

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a module that draw a pascal triangle """
+
+
+def pascal_triangle(n):
+    """ a passcal triangle """
+    if n <= 0:
+        return []
+    pascal = [[1]]
+
+    for i in range(1, n):
+        prev_row = pascal[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        pascal.append(new_row)
+    return pascal
