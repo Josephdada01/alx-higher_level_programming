5eba89fd5d8a2a9606d4054d0796b97353fd098f
Josephdada01 <ridwandada500@gmail.com>
2023-09-17 16:08:02 -0700

    omo

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+""" a class rectangle module, the class is inherited from base """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ a rectangle class that is inherited from base """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ an instance private attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setting width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ an instance private attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setting height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ retriving x with property """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ setting x """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ retriving y with property """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ setting y """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ a public instance method tha return the area value of Rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """ a public method that prints in stdout the Rectangle
+        instance with the character '#'
+        """
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """ string representation """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """ public method that assigns an argument to each attribute """
+        num_args = len(args)
+
+        if num_args >= 1:
+            self.id = args[0]
+        if num_args >= 2:
+            self.width = args[1]
+        if num_args >= 3:
+            self.height = args[2]
+        if num_args >= 4:
+            self.x = args[3]
+        if num_args >= 5:
+            self.y = args[4]
+        else:
+            """assigning values from kwargs to the attribute if doesent exit"""
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """ public method that returns the dictionary repr of rect """
+        return {
+                'id': self.id,
+                'width': self.width,
+                'height': self.height,
+                'x': self.x,
+                'y': self.y
+                }
