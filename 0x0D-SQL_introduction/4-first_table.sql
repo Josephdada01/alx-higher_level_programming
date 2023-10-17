8be51ef3029516c478a444acd32ab9f1cf5d0bea
Josephdada01 <ridwandada500@gmail.com>
2023-10-17 22:30:44 +0100

    fixing unneccessary code

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -5,7 +5,5 @@
 --The database name will be passed as an argument of the mysql command
 --If the table first_table already exists, your script should not fail
 --You are not allowed to use the SELECT or SHOW statements
-CREATE TABLE first_table IF NOT EXISTS (
-    'id' INT AUTO_INCREAMENT PRIMARY KEY,
-    'name' VARCHAR(256)
- );
+CREATE TABLE 'first_table' IF NOT EXISTS ('id' INT,
+    'name' VARCHAR(256));
