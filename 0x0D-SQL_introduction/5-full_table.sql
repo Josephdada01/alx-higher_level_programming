f466f9e4c3464d58df89ac12dbfc74e1ab256336
Josephdada01 <ridwandada500@gmail.com>
2023-10-18 00:49:26 +0100

    changing the line of code

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,7 @@
 -- script that prints the full description of the table 
 --first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW CREATE TABLE 'first_table';
+USE hbtn_0c_0;
+
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
