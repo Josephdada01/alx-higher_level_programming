da767f8f85cac7b852e4aa27c318edf89ff82f9a
Josephdada01 <ridwandada500@gmail.com>
2023-11-14 12:36:48 +0100

    using switchcase

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/**
+ * Write a script that prints 3 lines: (like 1-multi_languages.js)
+ * but by using an array of string and a loop
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ * You are not allowed to use any if/else statement
+ * You can use only one console.log
+ * You must use a loop (while, for, etc.)
+ */
+let i = 0;
+while (i <= 3) {
+  switch (i) {
+    case 1:
+      console.log('C is fun');
+      break;
+    case 2:
+      console.log('Python is cool');
+      break;
+    case 3:
+      console.log('JavaScript is amazing');
+      break;
+  }
+  i++;
+}
