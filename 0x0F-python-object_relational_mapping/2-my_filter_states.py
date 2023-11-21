1b92a1b3616cdae394a506567a36eff47de866c9
Josephdada01 <ridwandada500@gmail.com>
2023-11-21 09:13:07 +0100

    task 2 completed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+
+Your script should take 4 arguments: mysql username, mysql password,
+database name and state name searched (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+You must use format to create the SQL query with the user input
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+import MySQLdb
+import sys
+
+
+def search_state(username, password, database, state_name):
+    """take 4 arguments: mysql username, mysql password,
+    database name and state name searched
+    """
+    try:
+        con = MySQLdb.connect(host='localhost', port=3306,
+                              user=username, passwd=password, db=database)
+        cursor = con.cursor()
+        query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+        cursor.execute(query, (state_name,))
+        """fetching the results"""
+        states = cursor.fetchall()
+        """displaying the results"""
+        for state in states:
+            print(state)
+        cursor.close()
+        con.close()
+    except MySQLdb.Error as e:
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    username, password, database, state_name = sys.argv[1], sys.argv[2],
+    sys.argv[3], sys.argv[4]
+    search_state(username, password, database, state_name)
